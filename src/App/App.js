a65e8037d0b606145348d096949bd40d66efc5e7
John Achor <johnachor@gmail.com>
2018-11-27 21:18:18 -0600

    added column split and basic routing skeleton

--- src/App/App.js
+++ src/App/App.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, BrowserRouter, Redirect, Switch } from 'react-router-dom';
+import Sidebar from '../Components/Sidebar/Sidebar';
 import './App.css';
 
 class App extends Component {
-  render() {
+
+  state = {};
+
+  render () {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <BrowserRouter>
+          <div className="row">
+            <div className="col-3 sidebar-container">
+              <Sidebar />
+            </div>
+            <div className="col-9 resource-page-container">
+              <Switch>
+                <Route path="/" exact />
+              </Switch>
+            </div>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }

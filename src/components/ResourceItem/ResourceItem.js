ecf4ce5161286fca0c5ba11416b6da50809e6bbe
michael <michael.clark1992@gmail.com>
2018-11-27 20:32:17 -0600

    added ResourceItem and ResourceList

---
+++ src/components/ResourceItem/ResourceItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ResourceItem.css';
+
+class ResourceItem extends React.Component {
+  render () {
+    return (
+      <div className='ResourceItem'>
+
+      </div>
+    );
+  }
+};
+
+export default ResourceItem;
---
+++ src/components/ResourceList/ResourceList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ResourceList.css';
+
+class ResourceList extends React.Component {
+  render () {
+    return (
+      <div className='ResourceList'>
+
+      </div>
+    );
+  }
+};
+
+export default ResourceList;

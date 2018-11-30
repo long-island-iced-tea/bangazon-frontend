eff7179f77337f9fe3ba94ab57d81bdc5163b93d
michael <michael.clark1992@gmail.com>
2018-11-30 16:10:39 -0600

    added form based on objectModel

--- src/components/AddItemForm/AddItemForm.js
+++ src/components/AddItemForm/AddItemForm.js
@@ -10,9 +10,35 @@ class AddItemForm extends React.Component {
   }
 
   render () {
+    const {objectModel} = this.props;
+
+    const formInputs = Object.keys(objectModel).map(key => {
+
+      if (key.toLowerCase() === 'id') {
+        return null;
+      }
+
+      return (
+        <div key={key} className="form-group">
+          <label htmlFor={key}>{key}</label>
+          <input type="text" className="form-control" id={key} required />
+        </div>
+      );
+    });
+
     return (
       <div className='AddItemForm'>
-
+        <div className="card">
+          <div className="card-body">
+            <form>
+              {formInputs}
+              <button className="btn btn-default" type='submit'>Add Item</button>
+            </form>
+          </div>
+        </div>
+        <button className="btn">
+          <i className="fas fa-plus-circle"></i>
+        </button>
       </div>
     );
   }

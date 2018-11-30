3da067802f4d2b25fb9e62e913ee2b75d1750236
michael <michael.clark1992@gmail.com>
2018-11-30 15:20:57 -0600

    added proptypes

--- src/components/AddItemForm/AddItemForm.js
+++ src/components/AddItemForm/AddItemForm.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import './AddItemForm.css';
+import PropTypes from 'prop-types';
 
 class AddItemForm extends React.Component {
+
+  static propTypes = {
+    objectModel: PropTypes.object.isRequired,
+    addFunc: PropTypes.func.isRequired
+  }
+
   render () {
     return (
       <div className='AddItemForm'>

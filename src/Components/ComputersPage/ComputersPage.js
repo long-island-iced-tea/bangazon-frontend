e33a5314c684076e9207c663c0bfd7f4859778e1
michael <michael.clark1992@gmail.com>
2018-12-01 11:33:41 -0600

    gets items and has objectModel

--- src/Components/ComputersPage/ComputersPage.js
+++ src/Components/ComputersPage/ComputersPage.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import './ComputersPage.css';
+import apiAccess from '../../api-access/api';
+import AddItemForm from '../AddItemForm/AddItemForm';
+import ResourceList from '../ResourceList/ResourceList';
+
+const objectModel = {
+  id: 0,
+  purchasedAt: '',
+  decommissionedAt: '',
+  createdAt: '',
+  isNew: false,
+  isWorking: true
+};
 
 class ComputersPage extends React.Component {
+
+  state = {
+    items: []
+  }
+
+  componentDidMount() {
+    this.getItems();
+  }
+
+  getItems = () => {
+    apiAccess.apiGet('computer')
+      .then(res => {
+        this.setState({items: res.data});
+      });
+  }
+
   render () {
     return (
       <div className='ComputersPage'>
-
+        <AddItemForm objectModel={objectModel} />
+        <ResourceList resources={this.state.items} />
       </div>
     );
   }

4a58fec7cc0644859cf1345181a23b0c8a7f4490
Jessica Rodgers <jessica.rodgers11@gmail.com>
2018-12-01 11:16:33 -0600

    wip

--- src/components/SideNav/SideNav.js
+++ src/components/SideNav/SideNav.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './SideNav.css';
-// import PropTypes from 'prop-types';
+import NavButton from '../Index';
+
+// const buttonTitles = ['Customer', 'Orders', 'Product', 'Employee'];
+
+// for (let i = 0; i < buttonTitles.lenth; i++) {
+//   return <li>buttonTitles[i]</li>;
+// }
 
 class SideNav extends React.Component {
   render () {
     return (
       <div className="sidenav">
         <h1>Side Nav</h1>
+        <NavButton
+        />
       </div>
     );
   }

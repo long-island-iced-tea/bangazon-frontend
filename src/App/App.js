1686b6ba83601f433d80b949de7f4b6c273a1bba
michael <michael.clark1992@gmail.com>
2018-12-01 11:03:36 -0600

    added route and OrdersPage component

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import './App.css';
 
+import OrdersPage from '../components/OrdersPage/OrdersPage';
+
 class App extends Component {
 
   state = {};
@@ -12,11 +14,12 @@ class App extends Component {
         <BrowserRouter>
           <div className="row container-fluid">
             <div className="col-3 sidebar-container">
-              
+
             </div>
             <div className="col-9 resource-page-container">
               <Switch>
                 <Route path="/" exact />
+                <Route path="/orders" exact component={OrdersPage} />
               </Switch>
             </div>
           </div>
---
+++ src/Components/OrdersPage/OrdersPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './OrdersPage.css';
+
+class OrdersPage extends React.Component {
+  render () {
+    return (
+      <div className='OrdersPage'>
+
+      </div>
+    );
+  }
+};
+
+export default OrdersPage;

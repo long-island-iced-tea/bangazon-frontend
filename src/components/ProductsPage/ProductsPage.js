d9f72d40c5533b10113dd26142e411287573669e
michael <michael.clark1992@gmail.com>
2018-12-01 09:27:12 -0600

    added objectModel

--- src/components/ProductsPage/ProductsPage.js
+++ src/components/ProductsPage/ProductsPage.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import './ProductsPage.css';
+import '../../api-access/api';
+
+const objectModel = {
+  id: 0,
+  name: '',
+  description: '',
+  price: 0,
+  quantity: 0,
+  customerId: 0,
+  productType: 0
+};
 
 class ProductsPage extends React.Component {
   render () {

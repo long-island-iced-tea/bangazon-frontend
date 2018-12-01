73fcd6309b080ab98c309525a98a958bc7864cae
michael <michael.clark1992@gmail.com>
2018-12-01 11:29:43 -0600

    adds ComputersPage route

---
+++ src/Components/ComputersPage/ComputersPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ComputersPage.css';
+
+class ComputersPage extends React.Component {
+  render () {
+    return (
+      <div className='ComputersPage'>
+
+      </div>
+    );
+  }
+};
+
+export default ComputersPage;

d783df13e040965fe66095e6a385ea741e318536
michael <michael.clark1992@gmail.com>
2018-12-01 11:06:31 -0600

    gets items and added ResourceList

--- src/Components/OrdersPage/OrdersPage.js
+++ src/Components/OrdersPage/OrdersPage.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import './OrdersPage.css';
+import ResourceList from '../ResourceList/ResourceList';
+
+import apiAccess from '../../api-access/api';
 
 class OrdersPage extends React.Component {
+
+  state = {
+    items: []
+  }
+
+  componentDidMount() {
+    this.getItems();
+  }
+
+  getItems = () => {
+    apiAccess.apiGet('orders')
+      .then(res => {
+        this.setState({items: res.data});
+      });
+  }
+
   render () {
     return (
       <div className='OrdersPage'>
-
+        <ResourceList resources={this.state.items} />
       </div>
     );
   }

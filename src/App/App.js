fe155c8c764cd13e3f3cef374f84939520c05212
Amanda Slayton <alslayton86@gmail.com>
2018-12-01 11:10:47 -0600

    put route back in app.js

--- src/App/App.js
+++ src/App/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import ProductsPage from '../components/ProductsPage/ProductsPage';
 import ProductTypesPage from '../components/ProductTypesPage/ProductTypesPage';
 import TrainingProgramPage from '../Components/TrainingProgramPage/TrainingProgramPage';
+import EmployeePage from '../components/EmployeePage';
 
 class App extends Component {
 
@@ -22,6 +23,7 @@ class App extends Component {
                 <Route path="/products" exact component={ProductsPage} />
                 <Route path="/producttype" exact component={ProductTypesPage} />
                 <Route path="/trainingprogram" exact component={TrainingProgramPage} />
+                <Route path="/employee" exact component={EmployeePage} />
               </Switch>
             </div>
           </div>

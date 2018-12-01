f1ab91950739caf90ddfc89ba5412c40e3ce2367
michael <michael.clark1992@gmail.com>
2018-12-01 09:23:46 -0600

    added ProductsPage

---
+++ src/components/ProductsPage/ProductsPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ProductsPage.css';
+
+class ProductsPage extends React.Component {
+  render () {
+    return (
+      <div className='ProductsPage'>
+
+      </div>
+    );
+  }
+};
+
+export default ProductsPage;

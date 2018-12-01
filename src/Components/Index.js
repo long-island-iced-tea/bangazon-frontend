175c640a770a8828e4a434a7536edc7ddb631fc6
Amanda Slayton <alslayton86@gmail.com>
2018-12-01 09:28:42 -0600

    button

--- src/Components/Index.js
+++ src/Components/Index.js
@@ -1,60 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+function indexButton (props) {
+  return <button>${this.props.title}</button>;
+}
+
+
 class Navbar extends React.Component {
   render () {
 
     return (
-      <div>
-        <h1>INDEX</h1>
-        <ul class="navbar">
-          <li>
-            <Link to="/Customers">
-              Customers
-            </Link>
-          </li>
-          <li>
-            <Link to="/Employees">
-              Employees
-            </Link>
-          </li>
-          <li>
-            <Link to="/Products">
-              Products
-            </Link>
-          </li>
-          <li>
-            <Link to="/Computers">
-              Computers
-            </Link>
-          </li>
-          <li>
-            <Link to="/Departments">
-              Departments
-            </Link>
-          </li>
-          <li>
-            <Link to="/Orders">
-              Orders
-            </Link>
-          </li>
-          <li>
-            <Link to="PaymentTypes">
-            Payment Types
-            </Link>
-          </li>
-          <li>
-            <Link to="ProductTypes">
-            Product Types
-            </Link>
-          </li>
-          <li>
-            <Link to="TrainingPrograms">
-            Training Programs
-            </Link>
-          </li>
-        </ul>
-      </div>
+      <Link to="">
+        <indexButton title={this.props.title}/>
+      </Link>
     );
   }
 }

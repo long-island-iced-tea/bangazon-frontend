0f648ab13c9d183811b508036a3712e2be87fdc6
michael <michael.clark1992@gmail.com>
2018-12-01 10:49:47 -0600

    gets items, and adds new items

--- src/Components/ProductTypesPage/ProductTypesPage.js
+++ src/Components/ProductTypesPage/ProductTypesPage.js
@@ -1,11 +1,43 @@
 import React from 'react';
 import './ProductTypesPage.css';
+import apiAccess from '../../api-access/api';
+import ResourceList from '../ResourceList/ResourceList';
+import AddItemForm from '../AddItemForm/AddItemForm';
+
+const objectModel = {
+  id: 0,
+  name: ''
+};
 
 class ProductTypesPage extends React.Component {
+
+  state = {
+    items: []
+  }
+
+  componentDidMount() {
+    this.getItems();
+  }
+
+  getItems = () => {
+    apiAccess.apiGet('producttypes')
+      .then(res => {
+        this.setState({items: res.data});
+      });
+  }
+
+  addItem = (newItem) => {
+    apiAccess.apiPost('producttypes', newItem)
+      .then(res => {
+        this.getItems();
+      });
+  }
+
   render () {
     return (
       <div className='ProductTypesPage'>
-
+        <ResourceList resources={this.state.items}/>
+        <AddItemForm objectModel={objectModel} addFunc={this.addItem}/>
       </div>
     );
   }

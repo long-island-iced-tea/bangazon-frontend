298e99c8406feb358fef62fe706c05897fc60c33
michael <michael.clark1992@gmail.com>
2018-12-01 10:38:18 -0600

    added ProductTypesPage and route

---
+++ src/Components/ProductTypesPage/ProductTypesPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ProductTypesPage.css';
+
+class ProductTypesPage extends React.Component {
+  render () {
+    return (
+      <div className='ProductTypesPage'>
+
+      </div>
+    );
+  }
+};
+
+export default ProductTypesPage;

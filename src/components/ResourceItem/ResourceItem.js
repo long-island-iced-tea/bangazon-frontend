dd227e2b3f8175744dc24f75e5b07f14c8957c28
michael <michael.clark1992@gmail.com>
2018-12-04 18:38:37 -0600

    item is in state, and the forms can be edited

--- src/components/ResourceItem/ResourceItem.js
+++ src/components/ResourceItem/ResourceItem.js
@@ -5,17 +5,30 @@ import PropTypes from 'prop-types';
 
 class ResourceItem extends React.Component {
 
+  state = {
+    item: this.props.item,
+  }
+
   static propTypes = {
     item: PropTypes.object.isRequired,
     editFunc: PropTypes.func.isRequired,
     deleteFunc: PropTypes.func.isRequired,
   }
 
+  changeValue = (e) => {
+    const {item} = {...this.state};
+    item[e.target.id] = e.target.value;
+    this.setState({item})
+  }
+
   render () {
     const {item, editFunc, deleteFunc} = this.props;
 
     // Create table data from the values of the item
-    const itemProps = Object.values(item).map((v, i) => {
+    const itemProps = Object.entries(item).map((kvp, i) => {
+
+      var v = kvp[1];
+      var k = kvp[0];
 
       if (Array.isArray(v)) {
         return (<td key={i}>{'Count: ' + v.length}</td>);
@@ -24,7 +37,7 @@ class ResourceItem extends React.Component {
       }
       return (
         <td key={i}>
-          <input type="text" value={v} />
+          <input type="text" id={k} value={this.props.item[k]} onChange={this.changeValue}/>
         </td>
       );
 

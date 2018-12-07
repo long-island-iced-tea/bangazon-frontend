d1f7b972a699cee4f21671bf4e1dcd924128fa22
michael <michael.clark1992@gmail.com>
2018-12-07 16:21:03 -0600

    filters by makes and models

--- src/Components/ComputersPage/ComputersPage.js
+++ src/Components/ComputersPage/ComputersPage.js
@@ -43,9 +43,26 @@ class ComputersPage extends React.Component {
       });
   }
 
+  createModelOptions = (items) => {
+    this.models = items.reduce((models, computer) => {
+      if (!models.includes(computer.model) && computer.model != null) {
+        models.push(computer.model);
+      }
+      return models;
+    }, [])
+      .map(model => {
+        return (
+          <option key={model} value={model}>
+            {model}
+          </option>
+        )
+      });
+  }
+
   getItems = () => {
     apiAccess.apiGet('computer')
       .then(res => {
+        this.createModelOptions(res.data);
         this.createMakeOptions(res.data);
         this.setState({ items: res.data });
       });
@@ -76,11 +93,21 @@ class ComputersPage extends React.Component {
     });
   }
 
+  setModel = (e) => {
+    this.setState({
+      model: e.target.value,
+    });
+  }
+
   filterItems = (computer) => {
-    if (this.state.make != "") {
+    if (this.state.make !== "" && this.state.model !== "") {
+      return computer.make === this.state.make
+            && computer.model === this.state.model;
+    }
+    else if (this.state.make !== "") {
       return computer.make === this.state.make;
     }
-    else if (this.state.model != "") {
+    else if (this.state.model !== "") {
       return computer.model === this.state.model;
     }
     return computer;
@@ -94,6 +121,10 @@ class ComputersPage extends React.Component {
         <select id="make" onChange={this.setMake}>
           <option value="">Select a make:</option>
           {this.makes}
+        </select>
+        <select id="model" onChange={this.setModel}>
+          <option value="">Select a model:</option>
+          {this.models}
         </select>
         <ResourceList resources={this.state.items.filter(this.filterItems)} deleteFunc={this.deleteItem} editFunc={this.editComputer} />
         <AddItemForm objectModel={objectModel} addFunc={this.addItem} />

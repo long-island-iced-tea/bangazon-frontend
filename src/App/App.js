5041a19f9dec5fb3f3ca6829ca47825cfc457337
Amanda Slayton <alslayton86@gmail.com>
2018-12-11 21:23:20 -0600

    changes to styling to add color, header, footer

--- src/App/App.js
+++ src/App/App.js
@@ -22,24 +22,32 @@ class App extends Component {
       <div className="App">
         <BrowserRouter>
           <div className="container-fluid">
+            <header>
+              <h1 className="header-title">Bangazon Inc.</h1>
+            </header>
             <div className="row">
               <div className="col-3 sidebar-container">
                 <SideNav/>
               </div>
-              <div className="col-9 resource-page-container">
-                <Switch>
-                  <Route path="/" exact />
-                  <Route path="/orders" exact component={OrdersPage} />
-                  <Route path="/products" exact component={ProductsPage} />
-                  <Route path="/producttype" exact component={ProductTypesPage} />
-                  <Route path="/trainingprogram" exact component={TrainingProgramPage} />
-                  <Route path="/customer" exact component={CustomerPage} />
-                  <Route path="/employee" exact component={EmployeePage} />
-                  <Route path="/computers" exact component={ComputersPage} />
-                  <Route path="/departments" exact component={DepartmentsPage} />
-                  <Route path="/paymenttype" exact component={PaymentTypePage} />
-                </Switch>
+              <div className="main-container">
+                <div className="col-9 resource-page-container">
+                  <Switch>
+                    <Route path="/" exact />
+                    <Route path="/orders" exact component={OrdersPage} />
+                    <Route path="/products" exact component={ProductsPage} />
+                    <Route path="/producttype" exact component={ProductTypesPage} />
+                    <Route path="/trainingprogram" exact component={TrainingProgramPage} />
+                    <Route path="/customer" exact component={CustomerPage} />
+                    <Route path="/employee" exact component={EmployeePage} />
+                    <Route path="/computers" exact component={ComputersPage} />
+                    <Route path="/departments" exact component={DepartmentsPage} />
+                    <Route path="/paymenttype" exact component={PaymentTypePage} />
+                  </Switch>
+                </div>
               </div>
+            <footer>
+              <p className="footer-text">Achor, Clark, Rodgers, &amp; Slayton &copy;2018</p>
+            </footer>
             </div>
           </div>
         </BrowserRouter>
